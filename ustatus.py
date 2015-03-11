ab8ba89b0f95f09cb71baeec693b5216959646e5
Kevin Dungs <kevin@dun.gs>
2015-03-11 16:07:00 +0100

    Rewrite with less inheritance and more modularity.

--- ustatus.py
+++ ustatus.py
@@ -5,83 +5,103 @@ import psutil
 import rumps
 
 
+def default_format(status):
+    return '{}: {:3.0f}%'.format(status.name, status.query())
+
+
 class Status(object):
+    def __init__(self, name, query, format=default_format):
+        self._name = name
+        self.query = query
+        self._format = format
+
+    @property
+    def name(self):
+        return self._name
+
+    def format(self):
+        return self._format(self)
+
+
+class StatusCollection(Status):
+    def __init__(self, *statuses):
+        self.statuses = list(statuses)
+
     @property
     def name(self):
         pass
 
-    def get(self):
-        pass
-
-
-class Formatter(object):
     def format(self):
         pass
 
+    def update(self, index=None):
+        if not index:
+            for status in self.statuses:
+                status.update()
+        else:
+            self.statuses[index].update()
 
-class StatusFormatter(Formatter):
+
+class StatusComposer(StatusCollection):
+    def __init__(self, *statuses, **kwargs):
+        super(StatusComposer, self).__init__(*statuses)
+        self.separator = kwargs.get('separator', ' | ')
+
+    @property
+    def name(self):
+        return 'Composer'
+
     def format(self):
-        return '{}: {:3.0f}%'.format(self.name, self.get())
+        return self.separator.join(status.format() for status in self.statuses)
 
 
-class CpuStatus(Status, StatusFormatter):
+class StatusCycler(StatusCollection):
+    def __init__(self, *statuses, **kwargs):
+        super(StatusCycler, self).__init__(*statuses)
+        self.active = kwargs.get('active', len(self.statuses) - 1)
+
     @property
     def name(self):
-        return 'CPU'
+        return 'Cycler'
 
-    def get(self):
-        return psutil.cpu_percent()
+    def format(self):
+        return self.statuses[self.active].format()
 
-
-class RamStatus(Status, StatusFormatter):
-    @property
-    def name(self):
-        return 'RAM'
-
-    def get(self):
-        return psutil.phymem_usage().percent
-
-
-class StatusCycler(Status, StatusFormatter):
-    def __init__(self, *args):
-        self.statuus = list(args)
-        self.active_index = 0
-
-    @property
-    def active(self):
-        return self.statuus[self.active_index]
+    def update(self):
+        self.update(self.active)
 
     def next(self):
-        self.active_index = (self.active_index + 1) % len(self.statuus)
-
-    @property
-    def names(self):
-        return (s.name for s in self.statuus)
-
-    @property
-    def name(self):
-        return self.active.name
-
-    def get(self):
-        return self.active.get()
-
-
-class FormatterComposer(Formatter):
-    def __init__(self, *args, **kwargs):
-        self.separator = kwargs.get('separator', ' | ')
-        self.formatters = list(args)
-
-    def format(self):
-        return self.separator.join(f.format() for f in self.formatters)
+        self.active = (self.active + 1) % len(self.statuses)
 
 
 def set_title(app, title):
         app.title = title
 
+
+def configure_app(*statuses, **kwargs):
+    refresh = kwargs.get('refresh', 1)
+    composer = kwargs.get('composer', False)
+    sc = None
+    if composer:
+        sep = kwargs.get('separator', ' | ')
+        sc = StatusComposer(*statuses, separator=sep)
+    else:
+        sc = StatusCycler(*statuses)
+        cycle = kwargs.get('cycle', 5)
+        rumps.Timer(lambda _: sc.next(), cycle).start()
+    app = rumps.App(u"µStatus", title=sc.format())
+    rumps.Timer(lambda _: set_title(app, sc.format()), refresh).start()
+    return app
+
+
 if __name__ == '__main__':
-    #s = StatusCycler(RamStatus(), CpuStatus())
-    s = FormatterComposer(CpuStatus(), RamStatus(), separator=', ')
-    app = rumps.App(u"µStatus", title=s.format())
-    #rumps.Timer(lambda _: s.next(), 10).start()
-    rumps.Timer(lambda _: set_title(app, s.format()), 1).start()
-    app.run()
+    configure_app(
+        StatusComposer(
+            Status('CPU', lambda: psutil.cpu_percent()),
+            Status('CPU1', lambda: psutil.cpu_percent(0)),
+            Status('CPU2', lambda: psutil.cpu_percent(1)),
+            separator=', '
+        ),
+        Status('RAM', lambda: psutil.phymem_usage().percent),
+        Status('Disk /', lambda: psutil.disk_usage('/').percent),
+    ).run()
